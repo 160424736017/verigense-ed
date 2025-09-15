de3d18baae41917f261902f29d41153523af3724
160424736017 <160424736017@mjcollege.ac.in>
2025-09-15 11:38:12 +0530

    Added calender to the dashboard.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { ChartAreaInteractive } from "@/components/chart-area-interactive"
 import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
+import Calendar31 from "@/components/calendar-31"
 import {
   SidebarInset,
   SidebarProvider,
@@ -27,8 +28,13 @@ export default function Page() {
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 px-4 lg:px-6">
+                <div>
+                  <ChartAreaInteractive />
+                </div>
+                <div>
+                  <Calendar31 />
+                </div>
               </div>
               <DataTable data={data} />
             </div>
